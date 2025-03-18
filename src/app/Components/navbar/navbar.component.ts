8ab60c63b309ab75008c63fb4f068cfb9d6ae8c0
MBM3730 <160326122+mohamedmostafa3730@users.noreply.github.com>
2025-03-18 22:34:38 +0200

    update 0:2 => Enhance navbar and footer components with new styles and structure; add active link highlighting

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -3,7 +3,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
