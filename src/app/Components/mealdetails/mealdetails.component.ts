24e680596d3dc4fc729b3ec0552e602b71a93918
MBM3730 <160326122+mohamedmostafa3730@users.noreply.github.com>
2025-03-18 12:59:22 +0200

    update 0:1 => Add main layout components and routing configuration

---
+++ src/app/Components/mealdetails/mealdetails.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mealdetails',
+  imports: [],
+  templateUrl: './mealdetails.component.html',
+  styleUrl: './mealdetails.component.css'
+})
+export class MealdetailsComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { ServicesComponent } from './Components/services/services.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { NotfoundComponent } from './Components/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: "home", pathMatch: "full" },
+    { path: "home", component: HomeComponent, title: "home" },
+    { path: "about", component: AboutComponent, title: "about" },
+    { path: "services", component: ServicesComponent, title: "services" },
+    { path: "contact", component: ContactComponent, title: "contact" },
+    { path: "**", component: NotfoundComponent, title: "notfond" }
+];
